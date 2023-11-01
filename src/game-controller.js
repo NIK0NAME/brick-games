4d4a37824f17689f3a892859e55d3b3d0810eb0e
Nikolay Zabaleta <nikozabaleta@gmail.com>
2023-11-01 18:10:18 +0100

    refactor: move game global logic to game controller

--- src/game-controller.js
+++ src/game-controller.js
@@ -1,5 +1,22 @@
+import p5 from 'p5';
+import Menu from './menu';
+
 export default class GameController {
   constructor(sk) {
     this.sk = sk;
+    this.state = 'MENU';
+    this.menu = new Menu(this.sk);
+  }
+
+  draw() {
+    this.menu.draw();
+  }
+
+  handleKeyPress(keyCode) {
+    this.menu.handleMenuKeyPress(keyCode);
+  }
+
+  handleKeyReleased(keyCode) {
+    this.menu.handleMenuKeyReleased(keyCode);
   }
 }
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import 'p5/lib/addons/p5.sound';
 import Menu from './menu';
 import { KEYBOARD_KEYS } from './constants';
 import './style.css';
+import GameController from './game-controller';
 
 console.log('APP READY');
 
@@ -11,49 +12,53 @@ const consoleCase = { min: 320, max: 380 };
 const canvasInitialSize = { w: 251, h: 254 };
 const canvas = { w: canvasInitialSize.w, h: canvasInitialSize.h };
 
-const sketch = (sk) => {
-  const gameMenu = new Menu(sk);
-  // sk.preload = () => {
-  //   sk.soundFormats('wav');
-  //   sounds['lvlUp'] = loadSound('src/assets/lvl-up.wav');
-  //   sounds['hit'] = loadSound('src/assets/hit2.wav');
-  //   sounds['coin'] = loadSound('src/assets/coin.wav');
-  //   sounds['lose'] = loadSound('src/assets/lose.wav');
-  //   sounds['traffic'] = loadSound('src/assets/traffic.wav');
-  //   sounds['side'] = loadSound('src/assets/side.wav');
-  //   sounds['wall'] = loadSound('src/assets/wall.wav');
-  //   sounds['accelerate'] = loadSound('src/assets/accelerate2.wav');
-  // }
+function main() {
+  const sketch = (sk) => {
+    const gameController = new GameController(sk);
+    // sk.preload = () => {
+    //   sk.soundFormats('wav');
+    //   sounds['lvlUp'] = loadSound('src/assets/lvl-up.wav');
+    //   sounds['hit'] = loadSound('src/assets/hit2.wav');
+    //   sounds['coin'] = loadSound('src/assets/coin.wav');
+    //   sounds['lose'] = loadSound('src/assets/lose.wav');
+    //   sounds['traffic'] = loadSound('src/assets/traffic.wav');
+    //   sounds['side'] = loadSound('src/assets/side.wav');
+    //   sounds['wall'] = loadSound('src/assets/wall.wav');
+    //   sounds['accelerate'] = loadSound('src/assets/accelerate2.wav');
+    // }
 
-  sk.setup = () => {
-    sk.createCanvas(canvas.w, canvas.h);
-  }
+    sk.setup = () => {
+      sk.createCanvas(canvas.w, canvas.h);
+    }
 
-  sk.draw = () => {
-    gameMenu.draw();
-  }
+    sk.draw = () => {
+      gameController.draw();
+    }
 
-  sk.keyPressed = () => {
-    gameMenu.handleMenuKeyPress(sk.keyCode);
-  }
+    sk.keyPressed = () => {
+      gameController.handleKeyPress(sk.keyCode);
+    }
 
-  sk.keyReleased = () => {
-    gameMenu.handleMenuKeyReleased(sk.keyCode);
-  }
+    sk.keyReleased = () => {
+      gameController.handleKeyReleased(sk.keyCode);
+    }
 
-  document.querySelectorAll('.console-button').forEach((element) => {
-    element.addEventListener('click', () => {
-      switch (element.title) {
-        case 'left': gameMenu.handleKeyPress(KEYBOARD_KEYS.LEFT_ARROW); break;
-        case 'right': gameMenu.handleKeyPress(KEYBOARD_KEYS.RIGHT_ARROW); break;
-        case 'up': gameMenu.handleKeyPress(KEYBOARD_KEYS.UP_ARROW); break;
-        case 'down': gameMenu.handleKeyPress(KEYBOARD_KEYS.DOWN_ARROW); break;
-        case 'rotate': gameMenu.handleKeyPress(KEYBOARD_KEYS.ENTER); break;
-        case 'off': gameMenu.handleKeyPress(KEYBOARD_KEYS.ESCAPE); break;
-        case 'sp': gameMenu.handleKeyPress(KEYBOARD_KEYS.P_KEY); break;
-      }
+    document.querySelectorAll('.console-button').forEach((element) => {
+      element.addEventListener('click', () => {
+        switch (element.title) {
+          case 'left': gameController.handleKeyPress(KEYBOARD_KEYS.LEFT_ARROW); break;
+          case 'right': gameController.handleKeyPress(KEYBOARD_KEYS.RIGHT_ARROW); break;
+          case 'up': gameController.handleKeyPress(KEYBOARD_KEYS.UP_ARROW); break;
+          case 'down': gameController.handleKeyPress(KEYBOARD_KEYS.DOWN_ARROW); break;
+          case 'rotate': gameController.handleKeyPress(KEYBOARD_KEYS.ENTER); break;
+          case 'off': gameController.handleKeyPress(KEYBOARD_KEYS.ESCAPE); break;
+          case 'sp': gameController.handleKeyPress(KEYBOARD_KEYS.P_KEY); break;
+        }
+      });
     });
-  });
+  }
+
+  const P5 = new p5(sketch, 'canvas-container');
 }
 
-const P5 = new p5(sketch, 'canvas-container');
+main();
